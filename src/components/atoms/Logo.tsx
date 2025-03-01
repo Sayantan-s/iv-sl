fb9eaca2d1565d699a652f697c5c5ef19ef9c687
Sayantan <sayantan@hyresnap.com>
2025-03-01 21:25:38 +0530

    feat: added changes in dashboard

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,3 +1,7 @@
 export const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <div className="lg:w-12 lg:h-12 w-10 h-10 shrink-0 rounded-md shadow-md shadow-orange-500/20 flex items-center justify-center bg-gradient-to-br from-orange-500 via-orange-400 to-orange-500 font-bold text-gray-100">
+      S
+    </div>
+  );
 };
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,12 +1,14 @@
+import { Logo } from "@components/atoms/Logo";
 import { FC } from "react";
 
 export const Header: FC = () => {
   return (
-    <header className="text-gray-700">
+    <header className="text-gray-700 flex items-start gap-3">
+      <Logo />
       <div>
-        <h1 className="text-xl">Overview Dashboard</h1>
-        <p className="text-sm text-gray-400 mt-0.5">
-          Take look at the general sales and growth analytics
+        <h1 className="md::text-lg text-base">Overview Dashboard</h1>
+        <p className="md:text-sm text-xs text-gray-400">
+          General sales and growth analytics
         </p>
       </div>
     </header>
